522c1507a9485900987318bef8bbbb297f6bfc04
Tobi <crowniedavid@myself.com>
2019-12-30 00:49:30 +0000

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import App from 'next/app';
 import { ThemeProvider as StyledComponentsThemeProvider } from 'styled-components';
 import defaultTheme from '../src/config/defaultTheme';
+import { initGA, logPageView } from '../src/utils/analytics';
 
 export default class MyApp extends App {
+  componentDidMount() {
+    if (!window['GA_INITIALIZED']) {
+      initGA();
+      window['GA_INITIALIZED'] = true;
+    }
+    logPageView();
+  }
+
   render() {
     const { Component, pageProps } = this.props;
 
